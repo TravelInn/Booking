d1c64d3411b031b122ba4792827f00f264d81449
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-28 12:18:05 -0700

    updated server and db functions, changed artillery.io files

--- database/index.js
+++ database/index.js
@@ -14,7 +14,7 @@ module.exports.getRooms = id =>
 
 module.exports.getBookings = (id, start, end) =>
   client.query(
-    'SELECT foo.id, foo.price, foo.beds, bookings.startdate, bookings.enddate FROM (SELECT (id, price, beds) FROM rooms WHERE hostel_id=$1) AS foo JOIN bookings ON foo.id=bookings.room_id WHERE (bookings.startdate<=$3 AND bookings.startdate>=$2) OR (bookings.enddate<=$3 AND bookings.enddate>=$2)',
+    'SELECT foo.id, foo.price, foo.beds, bookings.startdate, bookings.enddate FROM (SELECT * from rooms WHERE hostel_id=$1) AS foo JOIN bookings ON foo.id=bookings.room_id WHERE (bookings.startdate<=$3 AND bookings.startdate>=$2) OR (bookings.enddate<=$3 AND bookings.enddate>=$2)',
     [id, start, end],
   );
 
--- server/server.js
+++ server/server.js
@@ -2,30 +2,16 @@ require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const request = require('request');
 const cors = require('cors');
 const db = require('../database/index');
 const helpers = require('./helpers');
-const Promise = require('bluebird');
 
-const app = express();
-
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(express.static(`${__dirname}/../public`));
-app.use('/:id', express.static(`${__dirname}/../public`));
-
-const port = 3003;
-const server = app.listen(port, () => {
-  console.log(`listening in on ${port}`);
-});
-
-app.get('/api/hostels/:hostelId/reservations', (req, res) => {
+const get = (req, res) => {
   const startDate = helpers.numberDate(req.query.start);
   const endDate = helpers.numberDate(req.query.end);
   let rooms = null;
-
-  db.getRooms(req.params.hostelId, startDate, endDate)
+  db.getRooms(req.params.hostelId)
     .then((result) => {
       rooms = result.rows;
       return db.getBookings(req.params.hostelId, startDate, endDate);
@@ -35,28 +21,70 @@ app.get('/api/hostels/:hostelId/reservations', (req, res) => {
       res.send(JSON.stringify(available));
     })
     .catch((err) => {
-      console.error(err);
+      res.statusCode = 503;
+      console.log(err);
       res.send(JSON.stringify(err));
     });
-});
+};
 
-app.post('/api/rooms/:roomId/bookings', (req, res) => {
+const post = (req, res) => {
   db.insertBooking(req.params.roomId, req.body.start, req.body.end)
     .then((result) => {
-      console.log('inserted', result);
-      res.send('inserted');
+      res.send(JSON.stringify(result));
     })
     .catch((err) => {
+      res.statusCode = 503;
       res.send(JSON.stringify(err));
     });
-});
+};
 
-app.put('/api/hostels/:hostelId/reservations', (req, res) => {
-  //Connect to database and do stuff
-});
+const createApp = () => {
+  const app = express();
+  app.use(cors());
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(express.static(`${__dirname}/../public`));
+  app.use('/:id', express.static(`${__dirname}/../public`));
 
-app.delete('/api/hostels/:hostelId/reservations', (req, res) => {
-  //connect to database and do stuff
+  app.get('/api/hostels/:hostelId/reservations', get);
+
+  app.post('/api/rooms/:roomId/bookings', post);
+
+  app.put('/api/hostels/:hostelId/reservations', (req, res) => {
+    //Connect to database and do stuff
+  });
+
+  app.delete('/api/hostels/:hostelId/reservations', (req, res) => {
+    //connect to database and do stuff
+  });
+  return app;
+};
+
+const app = createApp();
+// const app2 = createApp();
+
+const port = process.env.PORT || 3003;
+const server = app.listen(port, () => {
+  console.log(`listening in on ${port}`);
 });
+// const port2 = 4002;
+// const server2 = app2.listen(port2, () => {
+//   console.log(`listening in on ${port2}`);
+// });
+
+// const servers = ['http://localhost:4001', 'http://localhost:4002'];
+
+// let cur = 0;
+
+// const handler = (req, res) => {
+//   req.pipe(request({ url: servers[cur] + req.url })).pipe(res);
+//   cur = (cur + 1) % servers.length;
+// };
+
+// const loader = express();
+
+// loader.get('*', handler);
+// loader.post('*', handler);
+// const server = loader.listen(3003);
 
 module.exports = server;
